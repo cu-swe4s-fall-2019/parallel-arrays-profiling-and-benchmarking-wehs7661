231275dc950a295f1562a2396529145e1d2ac4e5
Wei-Tse Hsu <wehs7661@colorado.edu>
2019-10-30 23:45:55 -0600

    Developed unit tests for the searching method

---
+++ test_plot_gtex.py
@@ -0,0 +1,33 @@
+import unittest
+import plot_gtex
+
+class TestSearchingMethods(unittest.TestCase):
+    def test_linear_search(self):
+        L = [1, 2, 3, 4, 5]
+        r = plot_gtex.linear_search(3, L)
+        self.assertEqual(r, 2)
+
+        r = plot_gtex.linear_search(100, L)
+        self.assertEqual(r, -1)
+
+    def test_linear_search_hits(self):
+        L = ['a', 'b', 'b', 'a', 'a', 'a', 'b', 'a', 'a']
+
+        r = plot_gtex.linear_search_all_hits('a', L)
+        self.assertEqual(r, [0, 3, 4, 5, 7, 8])
+
+        r = plot_gtex.linear_search_all_hits('c', L)
+        self.assertEqual(r, [])
+
+    def test_binary_search(self):
+        L = [[1, 0], [2, 0], [3, 0], [4, 0], [5, 0]]
+
+        for i in range(1, 6):
+            r = plot_gtex.binary_search(i, L)
+            self.assertEqual(r, 0)
+
+        r = plot_gtex.binary_search(100, L)
+        self.assertEqual(r, -1)
+
+if __name__ == '__main__':
+    unittest.main()
